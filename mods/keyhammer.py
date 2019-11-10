001afc945978717f932c745245884893cd1e0661
Morten <mortenblaa@gmail.com>
2019-11-10 10:18:43 +0100

    Accidental conversion to all lowercase. Back in place now.

--- mods/keyhammer.py
+++ mods/keyhammer.py
@@ -14,13 +14,13 @@ import mods.data as data
 
 def do():
     # get main progress bar start progress
-    gmainprogressbar = mel.eval('$tmp = $gmainprogressbar')
-    cmds.progressbar(gmainprogressbar,
-                     e=true,
-                     beginprogress=true,
-                     isinterruptable=false,
-                     status='adding keyframes...',
-                     maxvalue=100)
+    gMainProgressBar = mel.eval('$tmp = $gMainProgressBar')
+    cmds.progressBar(gMainProgressBar,
+                     e=True,
+                     beginProgress=True,
+                     isInterruptable=False,
+                     status='Adding keyframes...',
+                     maxValue=100)
     
     # get selection
     if utils.is_graph_editor():
@@ -32,7 +32,7 @@ def do():
     # get curve functions
     curve_fns = []
     for curve_node in curves:
-        curve_fns.append(oma.mfnanimcurve(curve_node.object()))
+        curve_fns.append(oma.MFnAnimCurve(curve_node.object()))
     
     # get time range
     time_range = utils.get_time_slider_range()
@@ -42,45 +42,45 @@ def do():
     times = set()
     
     if is_range:
-        unit = om.mtime.uiunit()
-        min_time = om.mtime(time_range[0], unit)
-        max_time = om.mtime(time_range[1], unit)
+        unit = om.MTime.uiUnit()
+        min_time = om.MTime(time_range[0], unit)
+        max_time = om.MTime(time_range[1], unit)
         for curve_fn in curve_fns:
-            start_index = max(0, curve_fn.findclosest(min_time) - 1)  # -1 just to be safe
-            end_index = min(curve_fn.numkeys, curve_fn.findclosest(max_time) + 1)  # +1 just to be safe
+            start_index = max(0, curve_fn.findClosest(min_time) - 1)  # -1 just to be safe
+            end_index = min(curve_fn.numKeys, curve_fn.findClosest(max_time) + 1)  # +1 just to be safe
             for i in range(start_index, end_index):
                 times.add(curve_fn.input(i).value)
     else:
         for curve_fn in curve_fns:
-            for i in range(curve_fn.numkeys):
+            for i in range(curve_fn.numKeys):
                 times.add(curve_fn.input(i).value)
     
     # determine total number of operations
-    cmds.progressbar(gmainprogressbar, e=true, maxvalue=len(curve_fns))
+    cmds.progressBar(gMainProgressBar, e=True, maxValue=len(curve_fns))
     
-    # convert to mtime()
+    # convert to MTime()
     m_times = []
-    unit = om.mtime.uiunit()
+    unit = om.MTime.uiUnit()
     if is_range:
         for t in times:
             if time_range[0] <= t <= time_range[1]:
-                m_times.append(om.mtime(t, unit))
+                m_times.append(om.MTime(t, unit))
     else:
         for t in times:
-            m_times.append(om.mtime(t, unit))
+            m_times.append(om.MTime(t, unit))
     
     # add keys
     for curve_fn in curve_fns:
         ts = []
         vs = []
         for mt in m_times:
-            if curve_fn.find(mt) is none:
+            if curve_fn.find(mt) is None:
                 ts.append(mt)
                 vs.append(curve_fn.evaluate(mt))
         
         for t, v in zip(ts, vs):
-            curve_fn.addkey(t, v, change=data.anim_cache)
+            curve_fn.addKey(t, v, change=data.anim_cache)
         
-        cmds.progressbar(gmainprogressbar, e=true, step=1)
+        cmds.progressBar(gMainProgressBar, e=True, step=1)
     
-    cmds.progressbar(gmainprogressbar, e=true, endprogress=true)
+    cmds.progressBar(gMainProgressBar, e=True, endProgress=True)
